5b4900e63d4ed76d71407544c862650f9263c247
Sadık Efe Kartav <115922435+iamalreadynoob@users.noreply.github.com>
2022-11-12 12:16:44 +0300

    Create Identify.java

---
+++ textRecognize/Identify.java
@@ -0,0 +1,81 @@
+package textRecognize;
+
+public class Identify {
+	
+	private String[] rawText;
+	
+	public Identify(String[] rawText)
+	{
+		this.rawText = rawText;
+	}
+	
+	public String getRecognizableText(String filler)
+	{
+		return(recognize(filler));
+	}
+	
+	private String recognize(String filler)
+	{
+		String identified = "";
+		int indexI = 0;
+		int indexF = 0;
+		
+		for(int i = 0; i < rawText.length; i++)
+		{
+			if(rawText[i].charAt(0) == '"')
+			{
+				indexI = i;
+				break;
+			}
+		}
+		
+		for(int i = 0; i < rawText.length; i++)
+		{
+		
+			int length = rawText[i].length();
+			
+			if(rawText[i].charAt(length - 1) == '"')
+			{
+				indexF = i;
+			}
+			
+		}
+		
+		identified = wordSlicer(rawText[indexI], true) + filler;
+		
+		for(int i = indexI + 1; i < indexF; i++)
+		{
+			identified = identified + rawText[i] + filler;
+		}
+		
+		identified = identified + wordSlicer(rawText[indexF], false);
+		
+		
+		return identified;
+	}
+	
+	private String wordSlicer(String word, boolean isFirstLetter)
+	{
+		String sliced = "";
+		
+		if(isFirstLetter)
+		{	
+			for(int i = 1; i < word.length(); i++)
+			{
+				sliced += Character.toString(word.charAt(i));
+			}
+			
+		}
+		
+		else
+		{
+			for(int i = 0; i < word.length() - 1; i++)
+			{
+				sliced += Character.toString(word.charAt(i));
+			}
+		}
+		
+		return sliced;
+	}
+
+}
