261c4ae5b456ed77aa1e97a60400b787b2e38a84
Sadık Efe Kartav <115922435+iamalreadynoob@users.noreply.github.com>
2022-11-12 12:12:48 +0300

    Create Main.java

---
+++ mainPackage/Main.java
@@ -0,0 +1,14 @@
+package mainPackage;
+
+import java.io.IOException;
+
+import terminalGui.Screen;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException
+	{
+		new Screen();
+	}
+	
+}
